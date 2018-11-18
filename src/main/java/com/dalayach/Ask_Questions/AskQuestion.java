c7edc1c81c97948352cdb5545423cc538b46af80
dalayach <dalayach@masonlive.gmu.edu>
2018-11-17 23:29:56 -0500

    Initial commit

---
+++ src/main/java/com/dalayach/Ask_Questions/AskQuestion.java
@@ -0,0 +1,68 @@
+package com.dalayach.Ask_Questions;
+
+import java.util.Scanner;
+
+
+public class AskQuestion
+{
+
+   private Scanner scan = new Scanner(System.in);
+
+   void print(String message)
+   {//this method will be a godsend when it comes time to upgrade to V2
+    //at least, slightly so
+   
+      System.out.println(message);
+   
+   }
+   
+   String input()
+   {
+   
+      return this.scan.next();
+   
+   }
+   
+   boolean question(String prompt)
+   {
+   
+      boolean result = true;
+      boolean valid_response = false;
+      String choice;
+         
+      do
+      {
+         
+         print(prompt);
+         choice = input();            
+         
+         switch (choice.toLowerCase())
+         {
+         
+            case "y":
+            case "yes":
+               result = true;
+               valid_response = true;
+               break;
+               
+            case "n":
+            case "no":
+               result = false;
+               valid_response = true;
+               break;
+               
+            default:
+               print("INVALID RESPONSE\nTry again.\n\n");
+               break;
+               
+         
+         }
+      
+      }while(valid_response == false);
+   
+            
+      return result;
+   
+   }
+
+}
